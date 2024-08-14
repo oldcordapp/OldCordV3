634f8dcca2b1c6d9a6840cf0a5579d0cf06743a8
noia <ohayo@discord-doesnt.work>
2024-08-14 16:25:35 +1000

    Download logtext crash issue fix

--- api/download.js
+++ api/download.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
+const { logText } = require('../helpers/logger');
 
 const distributionFolder = path.join(__dirname, "..", "distribution");
 
