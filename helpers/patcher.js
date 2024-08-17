77c9efd73f2407f7dcf1381646921f43663d9e80
nulldg <55410457+nulldg@users.noreply.github.com>
2024-08-17 06:04:13 +0100

    Hard-code patcher_location from config

--- helpers/patcher.js
+++ helpers/patcher.js
@@ -3,18 +3,15 @@ const { logText } = require('./logger');
 const { config } = require('./globalutils');
 
 //Generate monkey patcher
-let patchFile = null;
-if (config.patcher_location !== "") {
-    if (!config.patcher_config) {
-        console.error("No patcher_config was provided in the config.");
-        process.exit(1);
-    }
-    
-    patchFile = "const patcher_config = ";
-    patchFile += JSON.stringify(config.patcher_config);
-    patchFile += ";\n";
-    patchFile += fs.readFileSync(config.patcher_location, "utf8");
+if (!config.patcher_config) {
+    console.error("No patcher_config was provided in the config.");
+    process.exit(1);
 }
+
+let patchFile = "const patcher_config = ";
+patchFile += JSON.stringify(config.patcher_config);
+patchFile += ";\n";
+patchFile += fs.readFileSync("./clients/assets/patcher/patch.js", "utf8");
 
 //Ensure patch config has all the keys it needs
 //TODO: IOU 1 patcher_config field
@@ -23,9 +20,6 @@ module.exports = {
     patchFile: patchFile,
     inject: (html) => {
         html = "<!DOCTYPE html>\n" + html;
-        if (!patchFile)
-            return html;
-        else
-            return html.replace("</body>", `   <script src="/patch.js"></script>\n   </body>`);
+        return html.replace("</body>", `   <script src="/patch.js"></script>\n   </body>`);
     },
 };
