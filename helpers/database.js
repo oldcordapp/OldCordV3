36a6bbd138e1748f5e8d306b75f648b26f67c280
noia <contact@noia.site>
2024-08-25 12:46:18 +1000

    Oops, one left

--- helpers/database.js
+++ helpers/database.js
@@ -332,11 +332,9 @@ const database = {
     },
     setPrivateChannels: async (user_id, private_channels) => {
         try {
-            const result = await database.runQuery(`
+            await database.runQuery(`
                 UPDATE users SET private_channels = $1 WHERE id = $2
             `, [JSON.stringify(private_channels), user_id]);
-
-            console.log('Database update result:', result);
 
             return true;
         } catch(error) {
